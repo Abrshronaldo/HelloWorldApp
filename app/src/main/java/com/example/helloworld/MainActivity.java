4c1effbdec24b027dfd2683612fc3e205d10a215
Abrshronaldo <abab61534@gmail.com>
2025-12-25 21:45:44 +0300

    d

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -175,7 +175,7 @@ t.setLayoutParams(Fparams);
 
 ConstraintLayout.LayoutParams kparams = new ConstraintLayout.LayoutParams(
 
-  448,
+  438,
   45
 );
 
@@ -226,7 +226,7 @@ lparams.leftMargin=11;
 lparams.bottomMargin=3;
 lparams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
 
-lparams.rightMargin=237;
+lparams.rightMargin=100;
 yo.setLayoutParams(lparams);
  
  
